ce58d79d25c2fa67895c90c2c338440aa304e83e
Ney <NeyComiunity@gmail.com>
2025-09-12 21:59:10 +0300

    fix: reload command message fix

--- src/main/java/com/ney/messages/command/sub/ReloadSubCommand.java
+++ src/main/java/com/ney/messages/command/sub/ReloadSubCommand.java
@@ -2,6 +2,7 @@ package com.ney.messages.command.sub;
 
 import com.ney.messages.NeyCustomMessages;
 import com.ney.messages.config.parser.CommandConfigParser;
+import com.ney.messages.util.HexColorUtil;
 import com.ney.messages.util.LoggerHelper;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
@@ -22,7 +23,7 @@ public class ReloadSubCommand implements SubCommand {
 
         plugin.reloadPlugin();
 
-        sender.sendMessage(subCommandConfig.messageSuccess());
+        sender.sendMessage(HexColorUtil.color(subCommandConfig.messageSuccess()));
         loggerHelper.info("Command", "Configuration reloaded by %s", sender.getName());
 
         return true;
